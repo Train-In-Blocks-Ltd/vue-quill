4188d743fd0cdeea6b8a6154e8ce2071e02e1c04
Joe <joe@joebailey.xyz>
2020-10-25 23:34:04 +0000

    Added image transformation stuff

--- src/Quill.vue
+++ src/Quill.vue
@@ -1,164 +1,182 @@
 <template>
-    <div>
-        <div class="ui attached segment" ref="quill" @click.prevent="focusEditor"></div>
-    </div>
+  <div>
+    <div class="ui attached segment" ref="quill" @click.prevent="focusEditor"></div>
+  </div>
 </template>
 
 <script>
-    import defaultsDeep from 'lodash.defaultsdeep'
-    import Quill from 'quill'
-    import GrammarlyInline from './formats/GrammarlyInline'
+  import defaultsDeep from 'lodash.defaultsdeep'
+  import Quill from 'quill'
+  import GrammarlyInline from './formats/GrammarlyInline'
+  import ImageCompress from 'quill-image-compress'
+  import * as mobileView from 'quill-mobile-view'
+  import { ImageDrop } from 'quill-image-drop-module'
+  import 'quill/dist/quill.snow.css'
+  import 'quill-mobile-view/dist/quill-mobile-view.css'
 
-    export default {
-        model: {
-            prop: 'content',
-        },
+  export default {
+      model: {
+          prop: 'content',
+      },
 
-        props: {
-            content: {},
+      props: {
+          content: {},
 
-            formats: {
-                type: Array,
-                default() {
-                    return []
-                },
-            },
+          formats: {
+              type: Array,
+              default() {
+                  return []
+              },
+          },
 
-            keyBindings: {
-                type: Array,
-                default() {
-                    return []
-                },
-            },
+          keyBindings: {
+              type: Array,
+              default() {
+                  return []
+              },
+          },
 
-            output: {
-                default : 'delta'
-            },
+          output: {
+              default : 'delta'
+          },
 
-            bus: {
-                default: false,
-            },
+          bus: {
+              default: false,
+          },
 
-            config: {
-                default() {
-                    return {}
-                },
-            },
-        },
+          config: {
+              default() {
+                  return {}
+              },
+          },
+      },
 
-        data() {
-            return {
-                editor: {},
-                defaultConfig: {
-                    modules: {
-                        toolbar: [
-                            ['bold', 'italic', 'underline'],
-                            [
-                               { 'list': 'ordered' }, { 'list': 'bullet' }
-                            ],
-                            [{ 'align': [] }],
-                        ],
+      data() {
+          return {
+              editor: {},
+              defaultConfig: {
+                  modules: {
+                    imageCompress: {
+                      quality: 0.5, // default
+                      maxWidth: 500, // default
+                      maxHeight: 500, // default
+                      imageType: 'image/jpeg', // default
+                      debug: true
                     },
-                    theme: 'snow',
-                },
+                    quillMobileView: {},
+                    imageDrop: true,
+                    toolbar: [
+                        ['bold', 'italic', 'underline'],
+                        [
+                            { 'list': 'ordered' }, { 'list': 'bullet' }
+                        ],
+                        mobileView.options
+                        [{ 'align': [] }],
+                    ],
+                  },
+                  theme: 'snow',
+              },
+          }
+      },
+
+      mounted() {
+          if (this.keyBindings.length) {
+              this.defaultConfig.modules.keyboard = {
+                  bindings: this.keyBindings.map((binding) => {
+                      if (binding.remove) return false
+                      return {
+                          key: binding.key,
+                          metaKey: true,
+                          handler: binding.method.bind(this),
+                      }
+                  })
+              }
+          }
+
+          if (this.config.modules && this.config.modules.toolbar) {
+              this.defaultConfig.modules.toolbar = []
+          }
+
+          Quill.register(GrammarlyInline)
+          Quill.register('modules/imageCompress', ImageCompress)
+          Quill.register('modules/quillMobileView', mobileView.module)
+          Quill.register('modules/imageDrop', ImageDrop)
+
+          this.editor = new Quill(this.$refs.quill, defaultsDeep(this.config, this.defaultConfig))
+
+          if (this.content && this.content !== '') {
+            if (this.output != 'delta') {
+                this.editor.pasteHTML(this.content)
+            } else {
+                this.editor.setContents(this.content)
             }
-        },
+          }
 
-        mounted() {
-            if (this.keyBindings.length) {
-                this.defaultConfig.modules.keyboard = {
-                    bindings: this.keyBindings.map((binding) => {
-                        if (binding.remove) return false
-                        return {
-                            key: binding.key,
-                            metaKey: true,
-                            handler: binding.method.bind(this),
-                        }
-                    })
-                }
-            }
+          this.editor.on('text-change', (delta, source) => {
+              this.$emit('text-change', this.editor, delta, source)
+              this.$emit('input', this.output != 'delta' ? this.editor.root.innerHTML : this.editor.getContents())
+          })
 
-            if (this.config.modules && this.config.modules.toolbar) {
-                this.defaultConfig.modules.toolbar = []
-            }
+          this.editor.on('selection-change', (range) => {
+              this.$emit('selection-change', this.editor, range)
+          })
 
-            Quill.register(GrammarlyInline)
+          if (this.bus) {
+              this.bus.$on('focus-editor', () => this.focusEditor())
+              this.bus.$on('set-content', (content) => this.editor.setContents(content))
+              this.bus.$on('set-html', (html) => {
+                  if (!html || html === '') return
 
-            this.editor = new Quill(this.$refs.quill, defaultsDeep(this.config, this.defaultConfig))
+                  this.editor.root.innerHTML = html
+              })
+          }
 
-            if (this.content && this.content !== '') {
-	            if (this.output != 'delta') {
-	                this.editor.pasteHTML(this.content)
-	            } else {
-	                this.editor.setContents(this.content)
-	            }
-            }
+          this.$on('focus-editor', () => this.focusEditor())
+          this.$on('set-content', (content) => this.editor.setContents(content))
+          this.$on('set-html', (html) => {
+              if (!html || html === '') return
 
-            this.editor.on('text-change', (delta, source) => {
-                this.$emit('text-change', this.editor, delta, source)
-                this.$emit('input', this.output != 'delta' ? this.editor.root.innerHTML : this.editor.getContents())
-            })
+              this.editor.root.innerHTML = html
+          })
 
-            this.editor.on('selection-change', (range) => {
-                this.$emit('selection-change', this.editor, range)
-            })
+          this.$nextTick(() => {
+              const selectors = ['button', '.ql-picker-label', '.ql-picker-item']
+              const toolbar = this.$el.querySelector('.ql-toolbar')
+              selectors.forEach((selector) => {
+                  toolbar.querySelectorAll(selector).forEach((element) => {
+                      element.tabIndex = -1
+                  })
+              })
+          })
+      },
 
-            if (this.bus) {
-                this.bus.$on('focus-editor', () => this.focusEditor())
-                this.bus.$on('set-content', (content) => this.editor.setContents(content))
-                this.bus.$on('set-html', (html) => {
-                    if (!html || html === '') return
+      methods: {
+          focusEditor(e) {
+              if (e && e.srcElement) {
+                  let classList = e.srcElement.classList,
+                      isSegment = false
 
-                    this.editor.root.innerHTML = html
-                })
-            }
+                  classList.forEach((className) => {
+                      if (className === 'segment') {
+                          isSegment = true
+                          return
+                      }
+                  })
 
-            this.$on('focus-editor', () => this.focusEditor())
-            this.$on('set-content', (content) => this.editor.setContents(content))
-            this.$on('set-html', (html) => {
-                if (!html || html === '') return
+                  if (!isSegment) return
+              }
 
-                this.editor.root.innerHTML = html
-            })
+              this.editor.focus()
+              this.editor.setSelection(this.editor.getLength()-1, this.editor.getLength())
+          }
+      },
 
-            this.$nextTick(() => {
-                const selectors = ['button', '.ql-picker-label', '.ql-picker-item']
-                const toolbar = this.$el.querySelector('.ql-toolbar')
-                selectors.forEach((selector) => {
-                    toolbar.querySelectorAll(selector).forEach((element) => {
-                        element.tabIndex = -1
-                    })
-                })
-            })
-        },
-
-        methods: {
-            focusEditor(e) {
-                if (e && e.srcElement) {
-                    let classList = e.srcElement.classList,
-                        isSegment = false
-
-                    classList.forEach((className) => {
-                        if (className === 'segment') {
-                            isSegment = true
-                            return
-                        }
-                    })
-
-                    if (!isSegment) return
-                }
-
-                this.editor.focus()
-                this.editor.setSelection(this.editor.getLength()-1, this.editor.getLength())
-            }
-        },
-
-        beforeDestroy() {
-            if (this.bus) {
-                this.bus.$off('focus-editor')
-                this.bus.$off('set-content')
-                this.bus.$off('set-html')
-            }
-        },
-    }
+      beforeDestroy() {
+          if (this.bus) {
+              this.bus.$off('focus-editor')
+              this.bus.$off('set-content')
+              this.bus.$off('set-html')
+          }
+      },
+  }
 </script>
